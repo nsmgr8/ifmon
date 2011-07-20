1d0540b60b41f3977ede89d1a5d9c7868170addb
Nasimul Haque <nasim.haque@gmail.com>
2011-07-20 13:00:43 +0600

    move gui app controller to gui package

--- main.py
+++ main.py
@@ -9,32 +9,8 @@
 #
 
 import sys
-import os
 
-from PySide.QtCore import QTranslator, QLocale
-from PySide.QtGui import QApplication, QMessageBox
-
-from gui.mainwindow import MainWindow
-from ifmon.ifmon import setup_db
+from gui import app
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    locale = QLocale.system().name()
-    trans = QTranslator()
-    trans.load('%s/locale/ifmon_%s' % (os.path.dirname(
-        os.path.realpath(__file__)), locale.lower()))
-    app.installTranslator(trans)
-    try:
-        setup_db()
-    except:
-        QMessageBox.critical(None, 'Database error',
-            'Could not find the database.\n'
-            'Please install it properly by running install.py on Ubuntu.\n'
-            'For other Linux distributions, check the requirements.',
-            QMessageBox.Ok)
-        sys.exit(app.exit())
-    else:
-        mw = MainWindow()
-        mw.show()
-    sys.exit(app.exec_())
-
+    app.main(sys.argv)
